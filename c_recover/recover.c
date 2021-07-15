692f48f391edfc23cf4b68154306663d05580724
chanszeyuen1992 <chanszeyuen1992@gmail.com>
2021-07-15 11:27:01 +0800

    add C proj, recover.

---
+++ c_recover/recover.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define BLOCK 512
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./recover image\n");
+        return 1;
+    }
+    
+    char *inputName = argv[1];
+    FILE *inputFile = fopen(inputName, "r");
+    if (inputFile == NULL)
+    {
+        printf("Usage: ./recover image\n");
+        return 1;
+    }
+    
+    unsigned char *buffer = malloc(BLOCK);
+    FILE *outputFile = NULL;
+    char fileName[8];
+    int fileNum = 0;
+    bool newImg = false;
+    
+    while (fread(buffer, BLOCK, 1, inputFile) == 1)
+    {
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
+        {
+            if (newImg == false)
+            {
+                newImg = true;
+            }
+            else
+            {
+                fclose(outputFile);
+            }
+            
+            sprintf(fileName, "%03i.jpg", fileNum);
+            outputFile = fopen(fileName, "w");
+            fileNum++;
+        }
+        if (newImg == true)
+        {
+            fwrite(buffer, BLOCK, 1, outputFile);
+        }
+    }
+    
+    fclose(inputFile);
+    fclose(outputFile);
+    return 0;
+}
